70cc095262c0a76cb346062f8418b5a904de0254
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-11-01 15:05:37 -0400

    steps

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express");  //step 2
+const cors = require("cors"); //step 3
 const app = express(); // step 4
 
 
@@ -9,13 +9,13 @@ app.use(cors());
 app.get("/api/users", (req, res) => {
     let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
     res.status(200).send(friends);
-  });
+  }); // step 7
 
 
 app.get("/weather/:temperature", (req, res) => {
     const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
     res.status(200).send(phrase);
-  });
+  });  // step 8
 
 
 
